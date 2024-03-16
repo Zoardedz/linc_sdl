3afc2dafbe3e0562ffd4b64bfb58aad0860e8cc9
YanniZ06 <102467588+YanniZ06@users.noreply.github.com>
2024-03-16 15:23:49 +0100

    Revert "GLContext utility functions"
    This reverts commit 5380919382f236318f7e8c91f18405376e0a1026.

--- sdl/GLContext.hx
+++ sdl/GLContext.hx
@@ -5,9 +5,4 @@ package sdl;
 @:include('linc_sdl.h')
 extern class GLContext {
     inline function isnull():Bool return this == untyped 0;
-    inline function compare(toCompare:GLContext):Bool { return (hndl(this) == hndl(toCompare)); }
-    inline function rawString():String { return '${hndl(this)}'; }
-
-    private inline function toString():String { return 'SDL_GLContext: ' + rawString(); }
-    private inline static function hndl(c:GLContext) { return untyped __cpp__('{0}', c); }
 }
